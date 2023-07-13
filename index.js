1d256fa601746f9f57ce3dfa28e36e4d1405f0d6
shahidmonowarr <shahidmonowar21@gmail.com>
2023-07-13 10:13:15 +0600

    [Done] Server Side

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
-const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.euxm4cs.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.exovlc7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,17 +18,17 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
   try {
-    const db = client.db('tech-net');
-    const productCollection = db.collection('product');
+    const db = client.db("tech-net");
+    const productCollection = db.collection("products");
 
-    app.get('/products', async (req, res) => {
+    app.get("/products", async (req, res) => {
       const cursor = productCollection.find({});
       const product = await cursor.toArray();
 
       res.send({ status: true, data: product });
     });
 
-    app.post('/product', async (req, res) => {
+    app.post("/product", async (req, res) => {
       const product = req.body;
 
       const result = await productCollection.insertOne(product);
@@ -36,15 +36,15 @@ const run = async () => {
       res.send(result);
     });
 
-    app.get('/product/:id', async (req, res) => {
+    app.get("/product/:id", async (req, res) => {
       const id = req.params.id;
 
       const result = await productCollection.findOne({ _id: ObjectId(id) });
-      console.log(result);
+      // console.log(result);
       res.send(result);
     });
 
-    app.delete('/product/:id', async (req, res) => {
+    app.delete("/product/:id", async (req, res) => {
       const id = req.params.id;
 
       const result = await productCollection.deleteOne({ _id: ObjectId(id) });
@@ -52,7 +52,7 @@ const run = async () => {
       res.send(result);
     });
 
-    app.post('/comment/:id', async (req, res) => {
+    app.post("/comment/:id", async (req, res) => {
       const productId = req.params.id;
       const comment = req.body.comment;
 
@@ -67,16 +67,16 @@ const run = async () => {
       console.log(result);
 
       if (result.modifiedCount !== 1) {
-        console.error('Product not found or comment not added');
-        res.json({ error: 'Product not found or comment not added' });
+        console.error("Product not found or comment not added");
+        res.json({ error: "Product not found or comment not added" });
         return;
       }
 
-      console.log('Comment added successfully');
-      res.json({ message: 'Comment added successfully' });
+      console.log("Comment added successfully");
+      res.json({ message: "Comment added successfully" });
     });
 
-    app.get('/comment/:id', async (req, res) => {
+    app.get("/comment/:id", async (req, res) => {
       const productId = req.params.id;
 
       const result = await productCollection.findOne(
@@ -87,11 +87,11 @@ const run = async () => {
       if (result) {
         res.json(result);
       } else {
-        res.status(404).json({ error: 'Product not found' });
+        res.status(404).json({ error: "Product not found" });
       }
     });
 
-    app.post('/user', async (req, res) => {
+    app.post("/user", async (req, res) => {
       const user = req.body;
 
       const result = await userCollection.insertOne(user);
@@ -99,7 +99,7 @@ const run = async () => {
       res.send(result);
     });
 
-    app.get('/user/:email', async (req, res) => {
+    app.get("/user/:email", async (req, res) => {
       const email = req.params.email;
 
       const result = await userCollection.findOne({ email });
@@ -116,8 +116,8 @@ const run = async () => {
 
 run().catch((err) => console.log(err));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
